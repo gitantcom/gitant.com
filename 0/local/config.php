1d077d271588f64b84fd13af7d9b5ab7a0dc12dd
lixin <lixin@sina.cn>
2013-11-01 15:58:38 +0800

    set password entire site

--- 0/local/config.php
+++ 0/local/config.php
@@ -2,17 +2,17 @@
 if (!defined('PmWiki')) exit();
 include_once("scripts/xlpage-utf-8.php");
 XLPage('zhcn','PmWikiZhCn.XLPage');
-$WikiTitle = "My New Wiki";
+$WikiTitle = "Gitant.Com";
 $PageLogoUrl = "http://example.com/mylogo.gif";
 
 # Uncomment these if needed
 #$ScriptUrl = 'http://example.com/pmwiki/pmwiki.php';
 #$PubDirUrl = 'http://example.com/pmwiki/pub';
 
-$DefaultPasswords['admin'] = crypt('onesecret');
+$DefaultPasswords['admin'] = crypt('021120');
 
 $EnableUpload = 1;
-$DefaultPasswords['upload'] = crypt('secrettwo');
+$DefaultPasswords['upload'] = crypt('021120');
 
 # Uncomment and change these if needed
 # putenv("TZ=EST5EDT"); # if you run PHP 5.0 or older
