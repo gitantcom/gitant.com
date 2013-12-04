d156ffa9f7fe2609327d6bfa67e4541372cdd458
uahoo <svngit@gmail.com>
2013-12-04 17:31:58 +0800

    init web

---
+++ 0/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+uahoo:$1$UBx3Xy2t$2omaOVruQzu9vlKapNrat0:lixin:uahoo@sina.com:admin,user
--- 0/index.php
+++ 0/index.php
@@ -1,3 +1,8 @@
 <?php
-	echo "Welcome to Baidu Cloud";
-?>
+/**
+ * Forwarder to doku.php
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
+ */
+header("Location: doku.php");
